356401f194807afc1c553bb5275dc0cdfeae1874
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-09-23 16:40:58 +0200

    Feature: Request Api data

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import pandas as pd
 import requests
 
-print("Wszystko jest gotowe!")
+API_KEY="25IO3T1D8V69J8DM"
+
+symbol="AAPL"
+
+url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}'
+
+response = requests.get(url)
+
+data = response.json()
+
+print(data)
