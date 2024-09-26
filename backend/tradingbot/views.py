632f50f9aee97990147d1a084feaf60f6e4cb8e3
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-09-26 15:23:24 +0200

    Feature: Set Up Views

--- backend/tradingbot/views.py
+++ backend/tradingbot/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Portfolio, Transaction
+from serializers import PortfolioSerializer, TransactionSerializer
 
-# Create your views here.
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+class PortfolioViewSet(viewsets.ModelViewSet):
+    queryset  = Portfolio.objects.all()
+    serializer_class = PortfolioSerializer
+
