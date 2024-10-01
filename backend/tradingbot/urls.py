b25d4f5fbf9190a0cafec61856138b3860e0860e
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-10-01 17:50:54 +0200

    Feature: Set Up Routing

--- backend/tradingbot/urls.py
+++ backend/tradingbot/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TransactionViewSet, PortfolioViewSet
+from .views import TransactionViewSet, PortfolioViewSet, PredictionViewSet
 
 router = DefaultRouter()
 router.register(r'transactions', TransactionViewSet)
 router.register(r'portfolio', PortfolioViewSet)
+router.register(r'predictions',PredictionViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
