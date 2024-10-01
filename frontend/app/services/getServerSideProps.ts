911d81dbbac5a5636a86170f7716fdc20fd72988
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-10-01 16:53:58 +0200

    Feature: Added API fetching functions for portfolio and transactions

--- frontend/app/services/getServerSideProps.ts
+++ frontend/app/services/getServerSideProps.ts
@@ -1,22 +1,17 @@
-// import { IPortfolio, ITransaction } from "../types";
-
 export async function getTransaction() {
-  // Fetch transactions
   const transactionsRes = await fetch("http://localhost:8000/transactions/");
   if (!transactionsRes.ok) {
-    throw new Error("Failed to fetch data");
+    throw new Error("Failed to fetch transactions");
   }
-  console.log(transactionsRes);
-
-  return transactionsRes.json();
+  const transactions = await transactionsRes.json();
+  return transactions;
 }
-// Fetch portfolio
+
 export async function getPortfolio() {
   const portfolioRes = await fetch("http://localhost:8000/portfolio/");
   if (!portfolioRes.ok) {
-    throw new Error("Failed to fetch data");
+    throw new Error("Failed to fetch portfolio");
   }
-  console.log(portfolioRes);
-
-  return portfolioRes.json();
+  const portfolio = await portfolioRes.json();
+  return portfolio;
 }
