8223cb9b82af5ebfcf4777ccdb0f7f2184a78ffb
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-10-01 17:50:02 +0200

    Feature:  Created the Prediction Model

--- backend/tradingbot/models.py
+++ backend/tradingbot/models.py
@@ -11,3 +11,9 @@ class Portfolio(models.Model):
     balance = models.FloatField()
     holdings = models.IntegerField()
     last_updated = models.DateTimeField(auto_now=True)
+
+class Prediction(models.Model):
+    date =models.DateTimeField(auto_now_add=True)
+    predicted_price = models.FloatField()
+    symbol = models.CharField(max_length=10)
+    confidence =models.FloatField(null=True, blank=True)
