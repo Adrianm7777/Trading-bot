c009dcd116251b87098af41f84e05384387b6ac3
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-10-04 17:39:42 +0200

    Feature: Added getPredictions function to get data from api

--- frontend/app/services/getServerSideProps.ts
+++ frontend/app/services/getServerSideProps.ts
@@ -1,5 +1,7 @@
 export async function getTransaction() {
-  const transactionsRes = await fetch("http://localhost:8000/transactions/");
+  const transactionsRes = await fetch(
+    "http://localhost:8000/api/transactions/"
+  );
   if (!transactionsRes.ok) {
     throw new Error("Failed to fetch transactions");
   }
@@ -8,10 +10,20 @@ export async function getTransaction() {
 }
 
 export async function getPortfolio() {
-  const portfolioRes = await fetch("http://localhost:8000/portfolio/");
+  const portfolioRes = await fetch("http://localhost:8000/api/portfolio/");
   if (!portfolioRes.ok) {
     throw new Error("Failed to fetch portfolio");
   }
   const portfolio = await portfolioRes.json();
   return portfolio;
+}
+export async function getPredictions() {
+  const predictionsRes = await fetch("http://localhost:8000/api/predictions/");
+  if (!predictionsRes.ok) {
+    throw new Error("Failed to fetch predictions");
+  }
+  const predictions = await predictionsRes.json();
+  console.log(predictions);
+
+  return predictions;
 }
