47017b453d904622304f87f58ff7f4a3db5c202c
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-10-07 16:44:48 +0200

    feature: Added endpoint to Django URLs

--- backend/tradingbot/urls.py
+++ backend/tradingbot/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TransactionViewSet, PortfolioViewSet, PredictionViewSet
+from .views import TransactionViewSet, PortfolioViewSet, PredictionViewSet, run_trading_bot
 
 router = DefaultRouter()
 router.register(r'transactions', TransactionViewSet)
@@ -8,5 +8,6 @@ router.register(r'portfolio', PortfolioViewSet)
 router.register(r'predictions',PredictionViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api/run-bot/', run_trading_bot, name='ru_bot')
 ]
