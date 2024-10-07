fdd4e338957ec2d6e7ad417abf769575ec5c5295
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-10-07 16:42:25 +0200

    Feature: Created an endpoint in Django that can handle the request and run the bot script

--- backend/tradingbot/views.py
+++ backend/tradingbot/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Portfolio, Transaction, Prediction
 from .serializers import PortfolioSerializer, TransactionSerializer, PredictionSerializer
+import subprocess
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 class TransactionViewSet(viewsets.ModelViewSet):
@@ -15,3 +18,11 @@ class PortfolioViewSet(viewsets.ModelViewSet):
 class PredictionViewSet(viewsets.ModelViewSet):
     queryset = Prediction.objects.all()
     serializer_class = PredictionSerializer
+
+@api_view(["POST"])
+def run_trading_bot(request):
+    try:
+        subprocess.Popen(["python", "Trading-bot\backend\main.py"])
+        return Response({"message": "Trading bot started successfully!"}, status=200)
+    except Exception as e:
+        return Response({"error": str(e)}, status=500)
